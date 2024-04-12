08f7121500bfd3e7739e079b8fc66bde30272a62
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-12 21:05:37 +0200

    Create Header, Navbar and NavLink components and import them in HomeLayout component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
     <>
         <nav>
+            <Header />
+            <Navbar />
             <span className="text-4xl text-primary">Confy</span>
         </nav>
         <Outlet />
