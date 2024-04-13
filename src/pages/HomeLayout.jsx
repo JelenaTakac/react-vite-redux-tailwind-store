b891aff77ff029158c4e0c5fa5a7ccb885b8f7b6
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-13 20:55:02 +0200

    Create Loading component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,5 @@
 import {Outlet} from "react-router-dom";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
+import { Header, Navbar, Loading } from "../components";
 
 const HomeLayout = () => {
   return (
