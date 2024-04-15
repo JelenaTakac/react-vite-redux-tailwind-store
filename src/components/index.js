1ff78848fe6ebd9627e685c720583684fb66d0c4
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-15 08:58:59 +0200

    Create FormInput component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as Loading } from "./Loading";
+export { default as FormInput } from "./FormInput";
