c27bdc9bfe7b4cc3b8bac5591b56e2fe78fc9c6e
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-18 12:18:08 +0200

    Add search input into Filter component

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-
-const FormInput = ({label, name, type, defaultValue}) => {
+const FormInput = ({label, name, type, defaultValue, size}) => {
   return (
     <div className="form-control w-full max-w-xs">
-        <label className="label">
+        <label htmlFor={name} className="label">
             <span className="label-text capitalize">{label}</span>
         </label>
         <input 
         type={type} 
         name={name} 
         value={defaultValue} 
-        className="input input-bordered w-full max-w-xs" />
+        className={`input input-bordered ${size}`} />
     </div>
   )
 }
