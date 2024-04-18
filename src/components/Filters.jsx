bd705b05dd1f3417eccc10844bff4bb054671763
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-18 15:51:11 +0200

    Create FormSelect component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,19 +1,44 @@
 import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 import { Form, useLoaderData, Link } from "react-router-dom";
 
-const Filters = ({size}) => {
-  return (
-    <div className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-        <FormInput 
-        type="search"
-        label="search product"
-        name="search"
-        size="input-sm"
-        />
-        <button type="submit" className="btn btn-primary btn-sm">search</button>
-        <Link to={"/products"} className="btn btn-accent btn-sm">reset</Link>
-    </div>
-  )
+const Filters = () => {
+    const { meta } = useLoaderData();
+
+    return (
+        <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+            <FormInput 
+            type="search"
+            label="search product"
+            name="search"
+            size="input-sm"
+            />
+
+            <FormSelect 
+            label="select category"
+            name="category"
+            list={meta.categories}
+            size="select-sm"
+            />
+
+            <FormSelect 
+            label="select comapny"
+            name="company"
+            list={meta.companies}
+            size="select-sm"
+            />
+
+            <FormSelect
+            label="sort by"
+            name="order"
+            list={["A-Z", "Z-A", "high", "low"]}
+            size="
+            select-sm"
+            />
+            <button type="submit" className="btn btn-primary btn-sm">search</button>
+            <Link to={"/products"} className="btn btn-accent btn-sm">reset</Link>
+        </Form>
+    )
 }
 
 export default Filters
---
+++ src/components/FormSelect.jsx
@@ -0,0 +1,23 @@
+const FormSelect = ({label, name, list, defaultValue, size}) => {
+  return (
+    <div>
+        <label htmlFor={name} className="label">
+            <span className="label-text capitalize">{label}</span>
+        </label>
+        <select 
+            name={name} 
+            id={name} 
+            value={defaultValue} 
+            className={`select select-bordered ${size}`}
+        >
+            {list.map(item => {
+                return (
+                    <option key={item} value={item}>{item}</option>
+                )
+            })}
+        </select>
+    </div>
+  )
+}
+
+export default FormSelect;
