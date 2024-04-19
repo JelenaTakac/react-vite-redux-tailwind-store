bde1d3495f15e9e568bd33a6b661c6310edc353b
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-19 10:48:02 +0200

    Setup params for filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,8 @@ import FormCheckbox from "./FormCheckbox";
 import { Form, useLoaderData, Link } from "react-router-dom";
 
 const Filters = () => {
-    const { meta } = useLoaderData();
+    const { meta, params } = useLoaderData();
+    const { search, company, category, order, price, shipping } = params;
 
     return (
         <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
@@ -14,6 +15,7 @@ const Filters = () => {
             label="search product"
             name="search"
             size="input-sm"
+            defaultValue={search}
             />
 
             <FormSelect 
@@ -21,6 +23,7 @@ const Filters = () => {
             name="category"
             list={meta.categories}
             size="select-sm"
+            defaultValue={category}
             />
 
             <FormSelect 
@@ -28,26 +31,29 @@ const Filters = () => {
             name="company"
             list={meta.companies}
             size="select-sm"
+            defaultValue={company}
             />
 
             <FormSelect
             label="sort by"
             name="order"
             list={["A-Z", "Z-A", "high", "low"]}
-            size="
-            select-sm"
+            size="select-sm"
+            defaultValue={order}
             />
 
             <FormRange 
             label="select price"
-            naem="price"
+            name="price"
             size="range-sm"
+            price={price}
             />
 
             <FormCheckbox 
             label="free shipping"
             name="shipping"
             size="checkbox-sm"
+            defaultValue={shipping}
             />
 
             <button type="submit" className="btn btn-primary btn-sm">search</button>
