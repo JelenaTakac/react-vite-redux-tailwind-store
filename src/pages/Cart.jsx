1a658eb8111485da5c21e92a715bd2959c8c9cdd
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-23 17:11:34 +0200

    Setup Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,41 @@
 import { useDispatch } from "react-redux";
 import { clearCart } from "../features/cart/cartSlice";
+import { useSelector } from "react-redux";
+import { CartItemsList, SectionTitle, CartTotals } from "../components";
+import { Link } from "react-router-dom";
 
 
 const Cart = () => {
+    const user = null;
+    const numItemsInCart = useSelector(state => state.cartState.numItemsInCart);
     const dispatch = useDispatch();
     
     const handleClearCart = () => {
         dispatch(clearCart());
     }
+
+    if (numItemsInCart === 0) {
+        return <SectionTitle text="Your cart is empty" />
+    }
+
     return (
-        <div>
-            <h1>Cart</h1>
+        <>
+            <SectionTitle text="Shopping Cart" />
+            <div className="mt-8 grid gap-8 lg:grid-cols-12">
+                <div className="lg:col-span-8">
+                    <CartItemsList />
+                </div>
+                <div className="lg:col-span-4 lg:pl-4">
+                    <CartTotals />
+                    {user ? (
+                        <Link to={"/checkout"} className="btn btn-primary btn-block mt-">Proceed to checkout</Link>
+                    ): (
+                        <Link to={"/login"} className="btn btn-primary btn-block mt-">Please login</Link>
+                    )}
+                </div>
+            </div>
             <button className="btn btn-primary" onClick={handleClearCart}>Clear cart</button>
-        </div>
+        </>
     )
 }
 
