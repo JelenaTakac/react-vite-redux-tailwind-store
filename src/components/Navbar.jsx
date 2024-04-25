4f2aee54c04d2e1560c73d8499293a6d8da15c97
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-25 14:25:35 +0200

    Move Theme logic from Navbar to userSlice

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,29 @@
-import { useState, useEffect } from "react";
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
-import { useSelector } from "react-redux";
-
-
-const themes = {
-    winter: "winter",
-    dracula: "dracula"
-}
-const getThemeFromLocalStorage = () => {
-    return localStorage.getItem("theme") || themes.winter;
-}
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../features/user/userSlice";
 
 const Navbar = () => {
-    const [theme, setTheme] = useState(getThemeFromLocalStorage());
-
     const numItemsInCart = useSelector(state => state.cartState.numItemsInCart);
+    const theme = useSelector(state => state.userState.theme);
+    const isDarkTheme = theme === "dracula";
 
+    const dispatch = useDispatch();
     const handleTheme = () => {
-        const {winter, dracula} = themes;
-        const newTheme = theme === winter ? dracula : winter;
-        setTheme(newTheme);
+        dispatch(toggleTheme())
     }
-
-    useEffect(() => {
-        document.documentElement.setAttribute("data-theme", theme);
-        localStorage.setItem("theme", theme);
-    }, [theme])
 
     return (
         <nav className="bg-base-200">
             <div className="navbar align-element ">
                 <div className="navbar-start">
-                    {/* Title */}
                     <NavLink
                         to="/"
                         className="hidden lg:flex btn btn-primary text-3xl items-center">
                         C
                     </NavLink>
-                    {/* DROPDOWN */}
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <FaBarsStaggered className="h-6 w-6" />
@@ -52,18 +35,11 @@ const Navbar = () => {
                     <ul className="menu menu-horizontal"><NavLinks /></ul>
                 </div>
                 <div className="navbar-end">
-                    {/* THEME ICONS */}
-                    <label className='swap swap-rotate '>
-                        {/* this hidden checkbox controls the state */}
-                        <input type='checkbox' onChange={handleTheme} />
-
-                        {/* sun icon */}
-                        <BsSunFill className='swap-on h-4 w-4' />
-
-                        {/* moon icon */}
-                        <BsMoonFill className='swap-off h-4 w-4' />
+                    <label className="swap swap-rotate">
+                        <input type="checkbox" onChange={handleTheme} defaultChecked={isDarkTheme} />
+                        <BsSunFill className="swap-on h-4 w-4" />
+                        <BsMoonFill className="swap-off h-4 w-4" />
                     </label>
-                    {/* CART LINK */}
                     <NavLink to="cart" className="btn btn-ghost btn-circle btn-md ml-4">
                         <div className="indicator">
                             <BsCart3 className="h-6 w-6" />
