662d3795b6f54f2f65943b562bbb038651faeafa
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-25 16:51:17 +0200

    Setup logoutUser reducer and access user in Header, NavLinks and Cart page

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const links = [
     {id: 1, url: "/", text: "home"},
@@ -10,18 +11,22 @@ const links = [
 ];
 
 const NavLinks = () => {
-  return (
-    <>
-        {links.map(link => {
-            const {id, url, text} = link;
-            return (
-                <li key={id}>
-                    <NavLink className="capitalize" to={url}>{text}</NavLink>
-                </li>
-            )
-        })}
-    </>
-  )
+    const user = useSelector(state => state.userState.user);
+
+    return (
+        <>
+            {links.map(link => {
+                const {id, url, text} = link;
+                if ((url === "checkout" || url === "orders") && !user) return null;
+                return (
+                    <li key={id}>
+                        <NavLink className="capitalize" to={url}>{text}</NavLink>
+                    </li>
+                )
+            })}
+        </>
+    )
 }
 
 export default NavLinks
+
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 
 const Cart = () => {
-    const user = null;
+    const user = useSelector(state => state.userState.user);
     const numItemsInCart = useSelector(state => state.cartState.numItemsInCart);
     const dispatch = useDispatch();
     
