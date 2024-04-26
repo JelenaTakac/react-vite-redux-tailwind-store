6050946fc9556541b10981e0d79ccd62f522b52c
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-26 15:28:03 +0200

    Create Checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, CartTotals } from "../components";
+
 
 const Checkout = () => {
+    const cartTotal = useSelector(state => state.cartState.cartTotal);
+
   return (
-    <div>Checkout</div>
+    <div>
+        {cartTotal === 0 ? (
+            <SectionTitle text="Your cart is empty" />
+        ) : (
+            <>
+                <SectionTitle text="Place your order" />
+                <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+                    <CheckoutForm />
+                    <CartTotals />
+                </div>
+            </>
+        )}
+    </div>
   )
 }
 
