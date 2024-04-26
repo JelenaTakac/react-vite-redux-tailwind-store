4f79f9b03f5e217ff7633f21b6335ab28826eab7
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-04-26 14:53:33 +0200

    Add functionality to login guest user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { FormInput, SubmitButton } from "../components";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
 import { redirect } from "react-router-dom";
 import { customFetch } from "../utils";
 import { toast } from "react-toastify";
@@ -16,7 +16,7 @@ export const action = (store) => async ({ request }) => {
         toast.success("Logged in successfully");
         return redirect("/");
     } catch (error) {
-        console.error(error);
+        console.log(error);
         const errorMessage = error?.response?.data?.error?.message || "please double check your credentials";
         toast.error(errorMessage);
         return null;
@@ -24,33 +24,51 @@ export const action = (store) => async ({ request }) => {
 }
 
 const Login = () => {
-  return (
-    <section className="h-screen grid place-items-center">
-        <Form 
-        method="POST" 
-        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
-            <h4 className="text-center text-3xl font-bold">Login</h4>
-            <FormInput 
-            type="email" 
-            label="email" 
-            name="identifier" 
-            defaultValue="test@test.com"/>
-            <FormInput 
-            type="password" 
-            label="password" 
-            name="password" 
-            defaultValue="secret" />
-            <div className="mt-4">
-                <SubmitButton text="login"/>
-            </div>
-            <button type="button" className="btn btn-secondary btn-block">Guest user</button>
-            <p className="text-center">
-                Not a member yet?
-                <Link to="/register" className="ml-2 link link-hover link-primary capitalize">regiser</Link>
-            </p>
-        </Form>
-    </section>
-  )
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const loginAsGuestUser = async () => {
+        try {
+            const response = await customFetch.post("/auth/local", {
+                identifier: "test@test.com",
+                password: "secret"
+            });
+            dispatch(loginUser(response.data));
+            toast.success("Welcome guest user");
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+            toast.error("Guest user login error. Please try later")
+        }
+    }
+
+    return (
+        <section className="h-screen grid place-items-center">
+            <Form 
+            method="POST" 
+            className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+                <h4 className="text-center text-3xl font-bold">Login</h4>
+                <FormInput 
+                type="email" 
+                label="email" 
+                name="identifier" 
+                />
+                <FormInput 
+                type="password" 
+                label="password" 
+                name="password" 
+                />
+                <div className="mt-4">
+                    <SubmitButton text="login"/>
+                </div>
+                <button type="button" className="btn btn-secondary btn-block" onClick={loginAsGuestUser}>Guest user</button>
+                <p className="text-center">
+                    Not a member yet?
+                    <Link to="/register" className="ml-2 link link-hover link-primary capitalize">regiser</Link>
+                </p>
+            </Form>
+        </section>
+    )
 }
 
 export default Login
